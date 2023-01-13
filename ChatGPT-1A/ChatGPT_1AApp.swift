ed63523f1725d2a38c4a48940fb9c7cff6e3d19b
“Charles”8675309 <thanes.buffs_0k@icloud.com>
2023-01-13 11:34:55 -0800

    Initial Commit

---
+++ ChatGPT-1A/ChatGPT_1AApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT_1AApp.swift
+//  ChatGPT-1A
+//
+//  Created by Charles Frush on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT_1AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
